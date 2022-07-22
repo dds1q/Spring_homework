dcad9babc0baa29da51e309b21c1863fb973621a
dds1q <dom955057@gmail.com>
2022-07-22 20:41:55 +0900

    Revert "웹개발의 봄, Spring 수업의 1주차 , 2주차 과제 업로드"
    This reverts commit 6e6eefa4760c91b37458f732dafdc8c68bf7399d.

--- week02_homework/src/main/java/com/sparta/week02_homework/Domain/FriendRepository.java
+++
@@ -1,6 +0,0 @@
-package com.sparta.week02_homework.Domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FriendRepository extends JpaRepository<Friend, Long> {
-}
