cccd613c222bd2461edaf36f5c041679926a1750
dds1q <dom955057@gmail.com>
2022-07-24 00:04:13 +0900

    3주차 과제 업로드

---
+++ week03/src/main/java/com/sparta/week03/domain/MemoRequestDto.java
@@ -0,0 +1,11 @@
+package com.sparta.week03.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class MemoRequestDto {
+    private final String username;
+    private final String contents;
+}
